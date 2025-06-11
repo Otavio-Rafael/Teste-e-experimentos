6ab23938c61bf7e447b4eb377c513e39d99c03f5
Otávio Rafael <otaviorafael.2001@gmail.com>
2025-06-11 15:01:33 -0300

    como organizar as pastas?

---
+++ Trabalhocon/Trabalhocon/Trabalhoconcessionaria/Trabalhoconcessionaria/Models/Veiculo.cs
@@ -0,0 +1,12 @@
+﻿namespace Trabalhoconcessionaria.Models
+{
+    public abstract class Veiculo//classe abstrata
+    {
+        public int Id { get; set; } // ID simulado (manual)
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public string Placa { get; set; }
+        public abstract string Exibirdados();
+    }
+}
